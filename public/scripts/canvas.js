f0f3a3324984cfe6205fe84f674a2ef675c4a99e
Nartuom <thomas.burton.lawl@gmail.com>
2020-01-21 15:57:58 +0000

    add canvas code good first draft- dots bounce and fall at different speeds

--- public/scripts/canvas.js
+++ public/scripts/canvas.js
@@ -15,28 +15,37 @@ function renderLoop(){
 requestAnimationFrame(renderLoop);
 
 
-var ctx = canvas.getContext("2d");
-
-function circles(x, y, dy) {
-	this.x = x;
-	this.y = y;
-	this.dy =dy;
+function circles(x, y, dy, f, fall,spd) {
+	this.x 	= x;
+	this.y 	=  y;
+	this.dy = dy;
+	this.spd = spd ;
 	this.draw = function() {
 		ctx.beginPath();
-   		ctx.arc(this.x, this.y, 5, 0, Math.PI *2, false);
-		ctx.strokeStyle = "#44978D";
-		ctx.fillStyle = "#44978D";
+		ctx.globalAlpha = f;
+   		ctx.arc(this.x, this.y, 2, 0, Math.PI *2, false);
+		ctx.strokeStyle = "rgba(68, 151, 141, 1)";
+		ctx.fillStyle = "rgba(68, 151, 141, 1)";
 		ctx.fill()
-		ctx.shadowColor = "#367870";
+		ctx.shadowColor = "rgba(68, 151, 141, 1)";
 		ctx.shadowBlur = 5; 	
 		ctx.stroke();
+		
 	}
 	this.update = function(){
-		if(this.y < 300){
-			this.dy = -this.dy;
+		if((this.y < maxY)&&(this.falling == true)){
+			this.dy = this.spd;
+			this.falling = true;
 		}
-		if(this.y > 700){
-			this.dy = -2;
+		if(this.y < minY){
+			this.dy = this.spd;
+			this.falling = true;
+		}
+		if(this.y > maxY){
+			this.dy = -1 * 2;
+			this.falling = false;
+			minY = 200;
+	
 		}
 		this.y += this.dy;
 		this.draw();
@@ -44,22 +53,26 @@ function circles(x, y, dy) {
 }
 
 var dotsArr =[]
+var minY = 100;
+var maxY = 800;
+var ctx = canvas.getContext("2d");
+var fall =true;
+var inc = 50;
+var fade  = 0.01;
 
-
-for(var i = 0; i < 400; i++){
-	var y = Math.floor(Math.random() * (700 - 300 + 1)+ 300);
-	var x = Math.random() * innerWidth;
-	dotsArr.push(new circles(x, y, -2));
-	// if((y > 200)&&(i < 30)){
-	// 	y -= 20;
-	// } else if((y < 800)&&(i > 30)){
-	// 	y += 20;
-	// }
+for(var i = 0; i < 350; i++){
+	var start = innerWidth/3;
+	var dy = (Math.random() * 3);
+	var spd = dy;
+	var y = Math.floor(Math.random() * (700 - 200 + 1)+ 300);
+	var x = Math.floor(start + inc);
 	
+	dotsArr.push(new circles(x, y, dy, fade, fall, spd));
+	inc  = inc + Math.random() * 5 + 1;
+	if(fade <1){
+		fade = fade + 0.005;
+	}	
 }
-
-
-
 
 function animate(){
 	requestAnimationFrame(animate);
