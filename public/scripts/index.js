70f42fc487b1ee15850c563dc5bf3e5b506fc594
Nartuom <thomas.burton.lawl@gmail.com>
2020-01-24 22:39:55 +0000

    Add css slide-in animation for skills and portfolio. Fix index.js checkSlide()

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,14 +1,9 @@
-
-
 const debounce = (func, wait, immediate) => {
 	let timeout;
-
 	return function() {
 		let context = this;
 		let args = arguments;
-
 		clearTimeout(timeout);
-
 		timeout = setTimeout(function() {
 			timeout = null;
 
@@ -22,26 +17,26 @@ const debounce = (func, wait, immediate) => {
 		}
 	};
 };
+//select Sliding elements on left
+var sliders = document.querySelectorAll(".slide-in");
 
-var sliders = document.querySelectorAll(".slide-in.slide-left");
-
-
+// Check if sliding elements are halfway on screen then add/ remove slide class.
 function checkSlide(){
-    sliders.forEach(function(slider){
+    sliders.forEach(function(slider, index ){
         //halfway through the image in relation to half the window
-    let slideInAt = (window.scrollY + window.innerHeight) - slider.offsetHeight / 2;
-    console.log(slideInAt);
-    //bottom of the image
-    let sliderBottom = slider.offsetTop + slider.offsetHeight;
-    console.log(sliderBottom);
-    //is the image half visible
-    let isHalfShown = slideInAt > slider.offsetTop;
-    console.log(isHalfShown);
-    //have we scrollled past the image
-    let isNotScrolledPast = window.scrollY < sliderBottom;
-    console.log(isNotScrolledPast);
-    //if image is half visible from to or bottom, then add active class
-
+        let slideInAt = (window.scrollY + window.innerHeight) - slider.offsetHeight / 2;
+        console.log(slideInAt);
+        //bottom of the image
+        let sliderBottom = slider.offsetTop + slider.offsetHeight;
+        console.log(sliderBottom);
+        //is the image half visible
+        let isHalfShown = slideInAt > slider.offsetTop;
+        console.log(isHalfShown);
+        //have we scrollled past the image
+        let isNotScrolledPast = window.scrollY < sliderBottom;
+        console.log(isNotScrolledPast);
+        //if image is half visible from to or bottom, then add active class
+        
         if(isHalfShown && isNotScrolledPast) {
             console.log("i see you")
             slider.classList.add("active");
@@ -52,8 +47,7 @@ function checkSlide(){
     });
 }   
 
-var myCheckSlideThrottle = debounce(checkSlide(), 10, true);
 
 window.addEventListener("scroll", function(){
-    debounce(checkSlide());
+    debounce(checkSlide(), 100, true);
 });
